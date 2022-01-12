28b61e872f34d31544f4b231c2fa79505204818a
Matic Conradi <matic.conradi@me.com>
2022-01-12 16:23:54 +0100

    Cleanup

--- app/components/ccxt.js
+++ app/components/ccxt.js
@@ -1,45 +1,40 @@
-const {
-	Exchange
-} = require("ticker-parser");
-const {
-	AbstractProvider
-} = require("./abstract.js");
-
+const { Exchange } = require("ticker-parser")
+const { AbstractProvider } = require("./abstract.js")
 
 class CCXT extends AbstractProvider {
 	static async request_candles(request) {
-		const ticker = request.ticker;
-		const exchange = Exchange.from_dict(ticker.exchange);
+		const ticker = request.ticker
+		const exchange = Exchange.from_dict(ticker.exchange)
 
-		if (!exchange) return [{}, ""];
+		if (!exchange) return [{}, ""]
 
-		var rawData;
+		var rawData
 
 		try {
 			rawData = await exchange.properties.fetchOHLCV(ticker.symbol, "1m", Date.now() - 3 * 60 * 1000)
-			if (rawData.length == 0 || !rawData[rawData.length - 1][4] || !rawData[0][1]) return [{}, ""];
+			if (rawData.length == 0 || !rawData[rawData.length - 1][4] || !rawData[0][1]) return [{}, ""]
 		} catch (err) {
-			return [{}, ""];
+			return [{}, ""]
 		}
 
 		let payload = {
 			candles: [],
 			title: ticker.name,
 			sourceText: "Data from " + exchange.name,
-			platform: "CCXT"
+			platform: "CCXT",
 		}
 
-		rawData.forEach(e => {
-			const timestamp = e[0] / 1000;
+		rawData.forEach((e) => {
+			const timestamp = e[0] / 1000
 			if (ticker.isReversed) {
-				payload.candles.push([timestamp, 1 / e[1], 1 / e[2], 1 / e[3], 1 / e[4]]);
+				payload.candles.push([timestamp, 1 / e[1], 1 / e[2], 1 / e[3], 1 / e[4]])
 			} else {
-				payload.candles.push([timestamp, e[1], e[2], e[3], e[4]]);
+				payload.candles.push([timestamp, e[1], e[2], e[3], e[4]])
 			}
-		});
+		})
 
-		return [payload, ""];
+		return [payload, ""]
 	}
 }
 
-module.exports = { CCXT };
+module.exports = { CCXT }
--- app/components/iexc.js
+++ app/components/iexc.js
@@ -1,37 +1,32 @@
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-const {
-	Exchange
-} = require("ticker-parser");
-const {
-	AbstractProvider
-} = require("./abstract.js");
-
+const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args))
+const { Exchange } = require("ticker-parser")
+const { AbstractProvider } = require("./abstract.js")
 
 class IEXC extends AbstractProvider {
 	static async request_candles(request) {
-		const ticker = request.ticker;
-		const exchange = Exchange.from_dict(ticker.exchange);
+		const ticker = request.ticker
+		const exchange = Exchange.from_dict(ticker.exchange)
 
-		var rawData;
+		var rawData
 
 		try {
-			const response = await fetch("https://cloud.iexapis.com/stable/stock/" + ticker.id + "/intraday-prices?chartLast=3&token=" + process.env.IEXC_KEY);
-			rawData = await response.json();
-			if (rawData.length == 0) return [{}, ""];
-			if (!ticker.quote && exchange) return [{}, "Price for `" + ticker.name + "` is not available on " + exchange.name + "."];
+			const response = await fetch("https://cloud.iexapis.com/stable/stock/" + ticker.id + "/intraday-prices?chartLast=3&token=" + process.env.IEXC_KEY)
+			rawData = await response.json()
+			if (rawData.length == 0) return [{}, ""]
+			if (!ticker.quote && exchange) return [{}, "Price for `" + ticker.name + "` is not available on " + exchange.name + "."]
 		} catch (err) {
-			return [{}, ""];
+			return [{}, ""]
 		}
 
 		let payload = {
 			candles: [],
 			title: ticker.name,
 			sourceText: "Data provided by IEX Cloud",
-			platform: "IEXC"
+			platform: "IEXC",
 		}
 
-		rawData.forEach(e => {
-			const timestamp = Date.parse(e.date + " " + e.minute) / 1000;
+		rawData.forEach((e) => {
+			const timestamp = Date.parse(e.date + " " + e.minute) / 1000
 
 			if (ticker.isReversed) {
 				if (e.marketClose) {
@@ -46,10 +41,10 @@ class IEXC extends AbstractProvider {
 					payload["candles"].push([timestamp, e.open, e.high, e.low, e.close])
 				}
 			}
-		});
+		})
 
-		return [payload, ""];
+		return [payload, ""]
 	}
 }
 
-module.exports = { IEXC };
+module.exports = { IEXC }
