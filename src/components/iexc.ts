ab5db6be33d345b8395bbabf42b70621c4827b2d
Matic Conradi <matic.conradi@me.com>
2023-05-05 16:48:58 +0200

    Revert, updated config

--- src/components/iexc.ts
+++ src/components/iexc.ts
@@ -15,7 +15,7 @@ export default class IEXC extends AbstractProvider {
 		} catch (err) {
 			console.error("Error occurred when fetching candles for", request.ticker.id, "from", request.ticker.exchange.id)
 			console.error(err)
-			console.error(response?.text())
+			console.error(response)
 			return [null, null]
 		}
 
