a879571ef2bc795d6ad18cffe3e73307f120c3a8
Matic Conradi <matic.conradi@me.com>
2023-10-03 19:42:16 +0200

    Bug fix

--- src/components/twelvedata.ts
+++ src/components/twelvedata.ts
@@ -14,7 +14,7 @@ export default class Twelvedata extends AbstractProvider {
 		try {
 			response = await fetch(`https://api.twelvedata.com/time_series?apikey=${process.env.TWELVEDATA_KEY}&interval=1min&type=${encodeURIComponent(request.ticker.metadata.type)}&symbol=${request.ticker.symbol}&exchange=${encodeURIComponent(request.ticker.exchange.name)}&timezone=utc&outputsize=3`)
 			rawData = await response.json()
-			if (rawData.values.length == 0) return { payload: null, message: null }
+			if (!rawData.values || rawData.values.length == 0) return { payload: null, message: null }
 			if (!request.ticker.quote) return { payload: null, message: "Price for `" + request.ticker.name + "` is not available on " + request.ticker.exchange.name + "." }
 		} catch (err) {
 			console.error("Error occurred when fetching candles for", request.ticker.symbol, "from", request.ticker.exchange.name)
